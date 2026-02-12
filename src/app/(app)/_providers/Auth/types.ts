8f01f5b385a89d4eb3d554316cb61b3f194b06a7
Daniel Wiśniewski <daniel.wisniewski@polskipcs.pl>
2026-02-12 20:51:55 +0100

    cleanup

--- src/app/(app)/_providers/Auth/types.ts
+++ src/app/(app)/_providers/Auth/types.ts
@@ -1,24 +1,24 @@
-import type { Permissions } from 'payload/auth'
+import type { Permissions } from 'payload'
 
 import type { User } from '../../../../payload-types'
 
- 
+
 export type ResetPassword = (args: {
   password: string
   passwordConfirm: string
   token: string
 }) => Promise<User>
 
-export type ForgotPassword = (args: { email: string }) => Promise<User>  
+export type ForgotPassword = (args: { email: string }) => Promise<User>
 
 export type Create = (args: {
   email: string
   firstName: string
   lastName: string
   password: string
-}) => Promise<User>  
+}) => Promise<User>
 
-export type Login = (args: { email: string; password: string }) => Promise<User>  
+export type Login = (args: { email: string; password: string }) => Promise<User>
 
 export type Logout = () => Promise<void>
 
@@ -30,6 +30,6 @@ export interface AuthContext {
   permissions?: null | Permissions
   resetPassword: ResetPassword
   setPermissions: (permissions: null | Permissions) => void
-  setUser: (user: null | User) => void  
+  setUser: (user: null | User) => void
   user?: null | User
 }
