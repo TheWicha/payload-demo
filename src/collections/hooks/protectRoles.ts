822fa6d6be5b6a3eaec5a149558f3a69265afc4f
Daniel Wiśniewski <daniel.wisniewski@polskipcs.pl>
2026-02-12 21:05:22 +0100

    common

--- src/collections/hooks/protectRoles.ts
+++ src/collections/hooks/protectRoles.ts
@@ -1,17 +1,19 @@
-import type { FieldHook } from 'payload'
+import type { FieldHook } from 'payload';
 
-import type { User } from '../../payload-types'
+import type { User } from '../../payload-types';
 
 // ensure there is always a `user` role
 // do not let non-admins change roles
 export const protectRoles: FieldHook<{ id: string } & User> = ({ data, req }) => {
-  const isAdmin = req.user?.roles.includes('admin') || data.email === 'demo@payloadcms.com' // for the seed script
+  const isAdmin =
+    (req.user?.roles && req.user?.roles.includes('admin')) ||
+    (data && data.email === 'demo@payloadcms.com'); // for the seed script
 
   if (!isAdmin) {
-    return ['user']
+    return ['user'];
   }
 
-  const userRoles = new Set(data?.roles || [])
-  userRoles.add('user')
-  return [...userRoles]
-}
+  const userRoles = new Set(data?.roles || []);
+  userRoles.add('user');
+  return [...userRoles];
+};
